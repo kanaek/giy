dcb2ccfa97da0d4078d07099912a1b25c043805a
kanaek <kanrun123@hotmail.com>
2016-02-16 13:57:02 +0800

    2

--- ownsvnpan/src/NewParser.java
+++ ownsvnpan/src/NewParser.java
@@ -38,25 +38,77 @@ public class NewParser {
     protected void reader(BufferedReader reader) throws IOException {
         String line;
         while ((line = reader.readLine()) != null) {
+            //System.out.println(line);
             parseLine(line);
         }
     }
 
     protected void parseLine(String line) {
         line = line.trim();
-        if (line.matches("\\[.*\\]")) {
-            currentSection = line.replaceFirst("\\[.*\\]", "$1");
+        if (line.matches("\\[.*\\]"))  {
+            System.out.println(line);
+            currentSection = line.replaceFirst("\\[(.*)\\]", "$1");
+            System.out.println(currentSection);
             current = new LinkedHashMap<String, String>();
-            sections.put(currentSection.trim(), current);
+            System.out.println(current);
+            //sections.put(currentSection, current);
         }  else if (line.matches(".*=.*") && !line.startsWith("#")) {
             if (current != null) {
                 for (String str : notes) {
                     int num = line.indexOf(str);
                     if (num !=-1) {
-
+                        line = line.substring(0,num);
                     }
+                }
+                if (line.length() > 0 && line.indexOf("=") != -1) {
+                    String[] kv = line.split("=",2);
+                    current.put(kv[0].trim(), kv[1].trim());
                 }
             }
         }
     }
+
+   public String getValue(String section, String name) {
+       Map<String,String> sectionmap = sections.get(section);
+
+       if (sectionmap == null) {
+           return null;
+       }
+       String value = sectionmap.get(name);
+       return value;
+   }
+
+   public Set<String> sectionKeys() {
+       return sections.keySet();
+   }
+
+   public Map<String,String> getValues(String section) {
+       return sections.get(section);
+   }
+
+   public boolean containsKey(String section, String key) {
+       Map<String,String> m = sections.get(section);
+       if (m != null) {
+           return m.get(key) == null;
+       }
+       return false;
+   }
+
+   public static void main(String[] args) {
+       try {
+           NewParser p = new NewParser("C:\\test\\svn.authz");
+           String val = p.getValue("groups", "RW__test");
+           Set<String> keys = p.sectionKeys();
+           for(String k : keys){
+               System.out.println("[" + k + "]");
+               Map<String, String> q = p.getValues(k);
+               for(Map.Entry<String, String> entry : q.entrySet()){
+                   System.out.println(entry.getKey() + "==" + entry.getValue());
+               }
+           }
+
+           }  catch (Exception e) {
+           e.printStackTrace();
+       }
+   }
 }
